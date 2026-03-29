e9308ff32edffc4b7d3b720c97f3b7aad1843e04
k022c0001-alt <k022c0001@g.neec.ac.jp>
2026-03-29 22:03:15 +0900

    Create SideMenu component with import and export buttons

---
+++ src/components/SideMenu.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const SideMenu = () => {
+    return (
+        <div className="side-menu">
+            <button>Import</button>
+            <button>Export</button>
+        </div>
+    );
+};
+
+export default SideMenu;
